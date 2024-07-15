1bcae59016347c49b9c5b9327825d754a9544910
olgaodc <olgaodincova@gmail.com>
2024-07-15 14:56:14 +0300

    Use like card code errors resolved

--- front-end/src/hooks/use-like-card.tsx
+++ front-end/src/hooks/use-like-card.tsx
@@ -1,28 +1,28 @@
 import { useState } from 'react';
-import { LocalStorageService } from '../services/local-storage-service';
+import LocalStorageService from '../services/local-storage-service';
 
 const CARDS_IDS = 'likedCards';
 
 const useLikeCard = () => {
-  const [likedCardsIds, setLikedCardsIds] = useState([]);
+  const [likedCardsIds, setLikedCardsIds] = useState<string[]>([]);
 
-  const changeCards = (createNewCards) => {
-    const ids = LocalStorageService.get(CARDS_IDS, []);
+  const changeCards = (createNewCards: (ids: string[]) => string[]) => {
+    const ids = LocalStorageService.get<string[]>(CARDS_IDS, []) || [];
     const newIds = createNewCards(ids);
     setLikedCardsIds(newIds);
     LocalStorageService.set(CARDS_IDS, newIds);
   };
 
-  const addLikedCard = (cardId) => {
-    changeCards((ids) => [...ids, cardId]);
+  const addLikedCard = (cardId: string) => {
+    changeCards((ids: string[]) => [...ids, cardId]);
   };
 
-  const removeLikedCard = (cardId) => {
-    changeCards((ids) => ids.filter((id) => id !== cardId));
+  const removeLikedCard = (cardId: string) => {
+    changeCards((ids: string[]) => ids.filter((id: string) => id !== cardId));
   };
 
-  const isCardLiked = (cardId) => {
-    const exisingLikedCards = LocalStorageService.get(CARDS_IDS, []);
+  const isCardLiked = (cardId: string) => {
+    const exisingLikedCards = LocalStorageService.get<string[]>(CARDS_IDS, []) || [];
     return exisingLikedCards.includes(cardId);
   };
 
--- front-end/src/services/local-storage-service.ts
+++ front-end/src/services/local-storage-service.ts
@@ -1,9 +1,18 @@
-const set = (key: string, data: any) => {
+const set = (key: string, data: any): void => {
   localStorage.setItem(key, JSON.stringify(data));
 };
 
-const get = (key: string, defaultData = null) => JSON.parse(localStorage.getItem(key))
-?? defaultData;
+const get = <T>(key: string, defaultData: T | null = null): T | null => {
+  const storedData = localStorage.getItem(key);
+  if (storedData) {
+    try {
+      return JSON.parse(storedData) as T;
+    } catch {
+      return defaultData;
+    }
+  }
+  return defaultData;
+};
 
 const LocalStorageService = {
   set,
