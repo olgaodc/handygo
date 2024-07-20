6dc2b51e650a302d19cda1e8b340f352e6fac76e
olgaodc <olgaodincova@gmail.com>
2024-07-20 03:54:55 +0300

    Dropdown created

--- front-end/src/components/avatar/avatar.tsx
+++ front-end/src/components/avatar/avatar.tsx
@@ -1,8 +1,12 @@
 import userAuth from '@/store/use-auth';
+import { useEffect, useRef, useState } from 'react';
 import styles from './styles.module.scss';
+import Dropdown from '../dropdown/dropdown';
 
 const Avatar = () => {
   const { user } = userAuth();
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   if (!user) {
     return (<div />);
@@ -10,8 +14,41 @@ const Avatar = () => {
 
   const userFirstLetter = user.name[0].toUpperCase();
 
+  const handleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClickOutside = (e: MouseEvent) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(e.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
+  const closeDropdown = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className={styles.avatar}>{userFirstLetter}</div>
+    <div className={styles.avatarWrapper} ref={dropdownRef}>
+      <button
+        className={styles.avatar}
+        type='button'
+        onClick={handleDropdown}
+      >
+        {userFirstLetter}
+      </button>
+      <div className={styles.dropdownWrapper}>
+        <Dropdown isOpen={isOpen} closeDropdown={closeDropdown} />
+      </div>
+
+    </div>
+
   );
 };
 
---
+++ front-end/src/components/dropdown/dropdown.tsx
@@ -0,0 +1,48 @@
+import clsx from 'clsx';
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import routes from '@/navigation/routes';
+import useAuth from '@/store/use-auth';
+import styles from './styles.module.scss';
+
+interface Props {
+  isOpen: boolean,
+  closeDropdown: () => void,
+}
+
+const Dropdown: FC<Props> = ({ isOpen = false, closeDropdown }) => {
+  const { logout } = useAuth();
+  return (
+    <div className={clsx(
+      styles.dropdown,
+      isOpen && styles.isOpen,
+    )}
+    >
+      <Link
+        className={styles.dropdownItem}
+        to={routes.SERVICES}
+        onClick={closeDropdown}
+
+      >
+        My Account
+      </Link>
+      <Link
+        className={styles.dropdownItem}
+        to={routes.SERVICES}
+        onClick={closeDropdown}
+      >
+        My Bookings
+      </Link>
+      <button
+        className={styles.dropdownItem}
+        type='button'
+        onClick={logout}
+      >
+        Logout
+      </button>
+    </div>
+
+  );
+};
+
+export default Dropdown;
