4fcb0f7261a211d3ab045495c12f2d69e92754aa
olgaodc <olgaodincova@gmail.com>
2024-08-22 03:17:13 +0300

    BaseUrl changed

--- front-end/src/services/api-service.ts
+++ front-end/src/services/api-service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
-// import PROD from '@/consts/environment';
+import PROD from '@/consts/environment';
 
-const baseURL = 'https://logoipsum-backend.onrender.com/';
+const baseURL = PROD ? 'https://logoipsum-backend.onrender.com/' : 'http://localhost:3001/';
 
 const ApiService = axios.create({
   baseURL,
