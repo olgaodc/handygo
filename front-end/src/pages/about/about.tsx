8f4ae4a66daec5ece37930659bae8fc390f4db80
olgaodc <olgaodincova@gmail.com>
2024-08-23 04:23:47 +0300

    Website logo updated

--- front-end/src/pages/about/about.tsx
+++ front-end/src/pages/about/about.tsx
@@ -23,12 +23,12 @@ const AboutPage = () => {
             <div className={styles.aboutWrapper}>
               <section className={styles.about}>
                 <h2 className={styles.aboutTitle}>About Us</h2>
-                <p className={styles.description}>Welcome to Logoipsum, your trusted partner for all home service and repair needs. We are dedicated to simplifying the process of maintaining your home by connecting you with the best local professionals. Our platform ensures you receive top-quality service with minimal effort, so you can enjoy a well-maintained home without the hassle.</p>
+                <p className={styles.description}>Welcome to HandyGo, your trusted partner for all home service and repair needs. We are dedicated to simplifying the process of maintaining your home by connecting you with the best local professionals. Our platform ensures you receive top-quality service with minimal effort, so you can enjoy a well-maintained home without the hassle.</p>
               </section>
               <section className={styles.history}>
                 <h2 className={styles.historyTitle}>Our History</h2>
                 <p>Our journey began with a simple vision: to revolutionize how homeowners find and book home services. Recognizing the challenges in locating reliable service providers, we aimed to create a platform that simplifies this process, ensuring top-quality home maintenance with minimal effort.</p>
-                <p>In 2018, a group of homeowners and tech enthusiasts founded Logoipsum. Experiencing the struggles of home maintenance themselves, they envisioned a user-friendly platform to connect homeowners with trustworthy local professionals. In 2019, we launched our beta version in New York City, offering cleaning, repair, and plumbing services. The positive response and growing demand led to an expansion of our service categories.</p>
+                <p>In 2018, a group of homeowners and tech enthusiasts founded HandyGo. Experiencing the struggles of home maintenance themselves, they envisioned a user-friendly platform to connect homeowners with trustworthy local professionals. In 2019, we launched our beta version in New York City, offering cleaning, repair, and plumbing services. The positive response and growing demand led to an expansion of our service categories.</p>
                 <p>From 2021, we enhanced our platform with real-time booking and we expanded to major cities across the United States, connecting more homeowners with top-rated service providers. Our dedication to excellence and customer satisfaction has earned us a reputation as a trusted partner for home maintenance.</p>
               </section>
             </div>
