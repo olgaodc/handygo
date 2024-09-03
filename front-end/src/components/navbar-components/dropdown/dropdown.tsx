73973a8e62dc4c50907fe2447bd9abced9a0f3cf
olgaodc <olgaodincova@gmail.com>
2024-09-04 00:09:03 +0300

    dropdown design changed

--- front-end/src/components/navbar-components/dropdown/dropdown.tsx
+++ front-end/src/components/navbar-components/dropdown/dropdown.tsx
@@ -3,6 +3,10 @@ import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '@/navigation/routes';
 import useAuth from '@/store/use-auth';
+import { ReactSVG } from 'react-svg';
+import UserIcon from '@/assets/user-icon.svg';
+import BookingIcon from '@/assets/book-icon.svg';
+import LogoutIcon from '@/assets/logout-icon.svg';
 import styles from './styles.module.scss';
 
 interface Props {
@@ -23,21 +27,24 @@ const Dropdown: FC<Props> = ({ isOpen = false, closeDropdown }) => {
         to={routes.ACCOUNT}
         onClick={closeDropdown}
       >
-        My Account
+        <ReactSVG className={styles.icon} src={UserIcon} />
+        <span>My Account</span>
       </Link>
       <Link
         className={styles.dropdownItem}
         to={routes.BOOKINGS}
         onClick={closeDropdown}
       >
-        My Bookings
+        <ReactSVG className={styles.icon} src={BookingIcon} />
+        <span>My Bookings</span>
       </Link>
       <button
         className={styles.dropdownItem}
         type='button'
         onClick={logout}
       >
-        Log Out
+        <ReactSVG className={styles.icon} src={LogoutIcon} />
+        <span>Log Out</span>
       </button>
     </div>
 
