b30e44eeaf8dd9488d371f819bc9459e0cafb7b0
olgaodc <olgaodincova@gmail.com>
2024-09-03 22:39:59 +0300

    unnecessary business info button deleted

--- front-end/src/components/business-components/business-info/business-info.tsx
+++ front-end/src/components/business-components/business-info/business-info.tsx
@@ -1,8 +1,5 @@
 import { FC } from 'react';
-import { ReactSVG } from 'react-svg';
 import { Business } from '@/types/business';
-import PrimaryButton from '@/components/primary-button/primary-button';
-import UploadIcon from '@/assets/upload-icon.svg';
 import LocationIcon from '@/assets/location-icon.svg';
 import MailIcon from '@/assets/mail-icon.svg';
 import UserIcon from '@/assets/user-icon.svg';
@@ -15,10 +12,6 @@ interface BusinessProps {
 }
 
 const BusinessInfo: FC<BusinessProps> = ({ business }) => {
-  const sendEmail = () => {
-    window.location.href = `mailto:${business.email}`;
-  };
-
   return (
     <div className={styles.businessInfo}>
       <img
@@ -34,13 +27,6 @@ const BusinessInfo: FC<BusinessProps> = ({ business }) => {
           <BusinessContact src={MailIcon}>{business.email}</BusinessContact>
         </div>
         <div className={styles.businessContacts}>
-          <PrimaryButton
-            variant='small'
-            onClick={sendEmail}
-            data-testid='send-email-button'
-          >
-            <ReactSVG src={UploadIcon} />
-          </PrimaryButton>
           <BusinessContact src={UserIcon} variant='special'>{business.person}</BusinessContact>
           <BusinessContact src={ClockIcon}>Available 8:00 AM to 10:00 PM</BusinessContact>
         </div>
